09c582c9aa73e71f53e046e9d918157e1c2c4fad
Alek-Grujic <alek.grujic001@outlook.com>
2025-06-06 12:54:49 +0200

    Track failed login attempts in User constructor

--- practice.js
+++ practice.js
@@ -27,40 +27,82 @@
 // -------------------------------------------------------------
 
 // 1. task
+
 // Public property - username
 // Private property - password
 // Metode - login
 // Object.defineProperty with get and set
 
+// function User(userName, password) {
+//     this.name = userName;
+//     let _password = password;
+
+//     Object.defineProperty(this, 'password', {
+//         get: function () {
+//             return "********";
+//         },
+//         set: function (value) {
+//             if (value.length < 6) { throw new Error('Password too short.') }
+//             _password = value;
+//         }
+//     });
+
+//     this.login = function (passCheck) {
+//         if (passCheck === _password) return 'Access granted';
+//         return 'Access denied';
+//     }
+
+// }
+
+// const user1 = new User('Mark', 'tajna123');
+
+// user1.password = 'nova123';
+
+// console.log(user1.password);
+
+// console.log(user1.login('tajna123'));
+
+
+// -------------------------------------------------------------
+
+
+// 2. task - number of logs
+
+
 function User(userName, password) {
     this.name = userName;
     let _password = password;
+    let _attempts = 0;
 
     Object.defineProperty(this, 'password', {
         get: function () {
             return "********";
         },
         set: function (value) {
-            if (value.length < 6) { throw new Error('To short...') }
+            if (value.length < 6) { throw new Error('Password too short.') }
             _password = value;
         }
     });
 
     this.login = function (passCheck) {
         if (passCheck === _password) return 'Access granted';
-        return 'Access denied';
+        if (_attempts <= 3) {
+            if (_attempts < 2) {
+                _attempts++;
+                return `Access denied ${_attempts}. time`
+            }
+            return 'Account locked';
+        }
     }
 
 }
 
-
 const user1 = new User('Mark', 'tajna123');
+
 user1.password = 'nova123';
 
 console.log(user1.password);
 
-console.log(user1.login('nova123'));
-
-
-
-
+console.log(user1.login('tajna123'));
+console.log(user1.login('tajna123'));
+console.log(user1.login('tajna123'));
